b6d5770e7bd68811cf7d5fc560340ca77dbf78b9
Christina Maimone <christina@Christinas-MacBook-Pro-2.local>
2020-12-17 17:12:40 -0600

    updating for change in source data

--- exercises/part4/data_cleaning_answers.R
+++ exercises/part4/data_cleaning_answers.R
@@ -10,6 +10,9 @@
 # you probably haven't seen yet.  It's a mini example of 
 # a real data analysis task
 
+# NOTE: sometimes the web page we're pulling data from changes;
+# if so, please let me know I need to update this!
+
 
 # install.packages("rvest")
 
@@ -26,54 +29,50 @@ tables <- html_nodes(page_html, "table")
 # double brackets because it's a list
 state_gdp <- html_table(tables[[3]]) 
 
-state_gdp
+# View the data to check what it looks like
+View(state_gdp)
 
 # We need to clean up the data so we can work with it
 
 # rename the columns to variable names that are appropriate for R
-names(state_gdp) <- c("rank", "state", "pop", "pop_percent", "gdp_per_capita", "region")
+names(state_gdp) <- c("rank", "state", "gdp", "perc", "gdp_per_capita", "region")
 
 # get rid of row for entire US
 state_gdp <- state_gdp[-1, ]
 
-# remove footnotes from data columns(things in [])
+# the only columns we want to keep are the name of the state/territory,
+# the gdp per capita, and the region.  Drop the rest
+state_gdp <- state_gdp[,c("state", "gdp_per_capita", "region")]
+
+# remove footnotes (things in []) where they exist
 # hint: gsub(thing/pattern to replace, replace it with, text)
 # Example: gsub("a", "!", "cat")
 # hint: "" is an empty string -- useful for removing things from text
 # hint: "\\[.{1,2}\\]" is a pattern to replace one or two characters inside []
-# hint: do a similar thing for each of the 3 columns that has footnotes
-state_gdp$pop_percent <- gsub("\\[.{1,2}\\]", "", state_gdp$pop_percent)
+# hint: do a similar thing for each of the columns with footnotes 
+# (2 that we care about last I checked)
 state_gdp$gdp_per_capita <- gsub("\\[.{1,2}\\]", "", state_gdp$gdp_per_capita)
 state_gdp$region <- gsub("\\[.{1,2}\\]", "", state_gdp$region)
 
-# remove commas from numbers and convert to numeric type
+# remove commas from numbers in gdp_per_capita and convert to numeric type
 # hint: gsub like above
 # hint: as.integer()
-state_gdp$pop <- as.integer(gsub(",", "", state_gdp$pop))
 state_gdp$gdp_per_capita <- as.integer(gsub(",", "", state_gdp$gdp_per_capita))
 
 
 # Now compute the mean per capita GDP for each region
 tapply(state_gdp$gdp_per_capita, state_gdp$region, mean)
+# you could have also used aggregate()
 
 
-# The above averages across states, treating each state equally.
-# Compute a mean weighted by population percent column (% of nation) instead with weighted.mean()
-# Hint: you can't use tapply or aggregate here, so compute for each region manually
-weighted.mean(state_gdp$gdp_per_capita[state_gdp$region == "Northeast"], 
-              state_gdp$pop_percent[state_gdp$region == "Northeast"])
-weighted.mean(state_gdp$gdp_per_capita[state_gdp$region == "Midwest"], 
-              state_gdp$pop_percent[state_gdp$region == "Midwest"])
-weighted.mean(state_gdp$gdp_per_capita[state_gdp$region == "South"], 
-              state_gdp$pop_percent[state_gdp$region == "South"])
-weighted.mean(state_gdp$gdp_per_capita[state_gdp$region == "West"], 
-              state_gdp$pop_percent[state_gdp$region == "West"])
 
-
-# optional: recode the "region" for the territories to a different value, and 
+# optional: recode the "region" for the territories to a different value, 
+# (instead have having them grouped in with the states) and 
 # recompute the mean per capital GDP for each region with the new definition
 # hint: 
-territories <- c("Puerto Rico", "Guam", "U.S. Virgin Islands", "Northern Mariana Islands", "American Samoa")
+territories <- c("Puerto Rico", "Guam", "U.S. Virgin Islands", 
+                 "Northern Mariana Islands", "American Samoa")
+# I'm going to treat DC as a state because it's between Virgina and Maryland
 
-state_gdp$region <- ifelse(state_gdp$state %in% territories, "territory", state_gdp$region)
+state_gdp$region[state_gdp$state %in% territories] <- "territory" 
 tapply(state_gdp$gdp_per_capita, state_gdp$region, mean)
